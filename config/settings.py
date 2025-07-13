709c70e3ac5008e81e2e236506c341a6c2e8f5ae
BoedZisan <stack1@birdsofeden.me>
2025-07-13 16:29:59 +0600

    Add gunicorn to requirements

--- config/settings.py
+++ config/settings.py
@@ -29,9 +29,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["*"]
+
 
 
 # Application definition
